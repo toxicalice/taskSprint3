41e09baefb414f208d2157654b8b346063c55955
toxicalIce <ad.digital@icloud.com>
2022-11-12 13:05:19 +0400

    Initial Commit

---
+++ taskSprint3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  taskSprint3
+//
+//  Created by toxicalIce on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var counter = 0
+
+    @IBOutlet weak var scoreLable: UILabel!
+    @IBOutlet weak var clickButton: UIButton!
+    
+    @IBAction func tapOnButton(_ sender: Any) {
+        counter += 1
+        scoreLable.text = "\(counter)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
